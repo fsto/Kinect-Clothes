685e0f001358ddbc934c8f048bb7b6dc8da8ad78
grotto <grottot@gmail.com>
2011-03-09 12:37:25 +0100

    Ny untracked image

--- Clothes/UserController.cpp
+++ Clothes/UserController.cpp
@@ -16,41 +16,6 @@ void UserController::drawJoint(XnSkeletonJointPosition& joint)
 	glVertex3f(joint.position.X, joint.position.Y, .1f);
 	glEnd();
 }
-
-void UserController::drawTrackedUser(KinectUser* g_userData)
-{
-	glPointSize(15.0f);
-	glLineWidth(8.0f);
-
-	if (g_userData)
-	{
-
- 		XnSkeletonJointPosition joint;
-
-		for(int i=0;i<Kinect::KINECT_JOINT_MAX;++i)
-		{
-			joint = g_userData->joints[i];
-//			printf("%d: %f,%f\n", i, joint.position.X, joint.position.Y);
-			drawJoint(joint);
-		}
-//		printf("\n");
-	}
-}
-
-/*
-* Draw a user that is not tracked. The only available point is CoM.
-*/
-void UserController::drawNewUser(KinectUser* g_userData)
-{
-	glPointSize(15.0f);
-	const XnPoint3D *com = &g_userData->centerOfMass;
-   	glColor3f(0.66, 0.33, 0.33);
-	glBegin(GL_POINTS);
-		glVertex3f(com->X, com->Y, 0.1f);
-	glEnd();
-}
-
-
 Garment* g;
 Garment* g2;
 
@@ -101,11 +66,78 @@ void drawTexture(KinectUser* user)
 	glDisable(GL_TEXTURE_2D);
 }
 
+void UserController::drawTrackedUser(KinectUser* user)
+{
+	glPointSize(15.0f);
+	glLineWidth(8.0f);
+
+	if (user)
+	{
+
+ 		XnSkeletonJointPosition joint;
+
+		for(int i=0;i<Kinect::KINECT_JOINT_MAX;++i)
+		{
+			joint = user->joints[i];
+//			printf("%d: %f,%f\n", i, joint.position.X, joint.position.Y);
+			drawJoint(joint);
+		}
+		drawTexture(user);
+//		printf("\n");
+	}
+}
+
+/*
+* Draw a user that is not tracked. The only available point is CoM.
+*/
+void UserController::drawNewUser(KinectUser* user)
+{
+	//draw CoM
+	glPointSize(15.0f);
+	const XnPoint3D *com = &user->centerOfMass;
+   	glColor3f(0.66, 0.33, 0.33);
+	glBegin(GL_POINTS);
+		glVertex3f(com->X, com->Y, 0.1f);
+	glEnd();
+
+	//draw untracked image
+	glDisable(GL_COLOR_MATERIAL);
+	glColor3f(1,1,1);
+	if(!g)
+	{
+		g = new Garment("skin/untracked.png");
+	}
+	XnFloat x = user->centerOfMass.X;
+	XnFloat y = user->centerOfMass.Y;
+	glPushMatrix();
+	//glTranslatef(0,-30,0);
+
+	g->bindTexture();
+	glEnable(GL_TEXTURE_2D);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0,1);
+	glVertex2f(x-80,y-80);
+	glTexCoord2f(0,0);
+	glVertex2f(x-80,y+80);
+	glTexCoord2f(1,0);
+	glVertex2f(x+80,y+80);
+	glTexCoord2f(1,1);
+	glVertex2f(x+80,y-80);
+	glEnd();
+
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+
+}
+
+
+
 void UserController::drawUser(KinectUser* g_userData)
 {
 	if (g_userData)
 	{
-		drawTexture(g_userData);
+//		drawTexture(g_userData);
 		if (g_userData->status & Kinect::USER_TRACKING)
 			drawTrackedUser(g_userData);
 		else
